72bf2d8bf5aad1de1536467776c8717d95a7d176
just-some-entity <entity@jsentity.dev>
2026-08-03 09:37:41 +0200

    remove constraints on property constraints

--- library/src/machine/resource/constraints.rs
+++ library/src/machine/resource/constraints.rs
@@ -1,17 +1,17 @@
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Default, Clone, PartialEq)]
 pub struct NumericConfigPropertyConstraints<T: Copy + PartialOrd> {
     pub min: Option<T>,
     pub max: Option<T>,
 }
 
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Default, Clone, PartialEq)]
 pub struct StringConfigPropertyConstraints {
     pub min_length: Option<usize>,
     pub max_length: Option<usize>,
     pub pattern: Option<String>,
 }
 
-#[derive(Debug, Default, Clone, PartialEq)]
-pub struct EnumConfigPropertyConstraints<T: PartialEq + ToString> {
+#[derive(Default, Clone, PartialEq)]
+pub struct EnumConfigPropertyConstraints<T: PartialEq> {
     pub allowed: Vec<T>,
 }
