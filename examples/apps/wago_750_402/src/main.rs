43df66bec4572e30328ef79b813bd4c3e614daa6
Snacj <0xSnacj@proton.me>
2026-09-01 12:29:07 +0200

    Changed downcasting of terminal

--- examples/apps/wago_750_402/src/main.rs
+++ examples/apps/wago_750_402/src/main.rs
@@ -3,7 +3,6 @@ use qitech_framework::TuiConfiguration;
 use qitech_framework::machine::ActResult;
 use qitech_framework::machine::BuildContext;
 use qitech_framework::machine::BuildResult;
-use qitech_framework::machine::ConfigProperty;
 use qitech_framework::machine::Machine;
 use qitech_framework::machine::MachineBuild;
 use qitech_framework::machine::StateProperty;
@@ -12,11 +11,12 @@ use qitech_framework::run_with_tui;
 use qitech_framework::runtime::EtherCATConfig;
 use qitech_framework::runtime::RuntimeConfiguration;
 use qitech_lib::ethercat_hal::devices::EthercatDevice;
-use qitech_lib::ethercat_hal::devices::wago_modules::wago_750_402::Wago750_402;
 use qitech_lib::ethercat_hal::devices::wago_modules::wago_750_354::Wago750_354;
-use qitech_lib::ethercat_hal::io::digital_output::DigitalOutputDevice;
+use qitech_lib::ethercat_hal::devices::wago_modules::wago_750_402::Wago750_402;
+use qitech_lib::ethercat_hal::io::digital_input::DigitalInputDevice;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::time::Duration;
 
 #[tokio::main]
 pub async fn main() {
@@ -31,27 +31,28 @@ pub async fn main() {
 #[derive(Machine)]
 pub struct Wago750_402Machine {
     coupler: Rc<RefCell<Wago750_354>>,
-    leds: [StateProperty<bool>; 4],
+    inputs: [StateProperty<bool>; 4],
 }
 
-impl Machine for Wago750_402Machine {}
+impl Machine for Wago750_402Machine {
+    fn act(&mut self, _dt: Duration) -> ActResult {
+        let coupler = self.coupler.borrow();
+        let wago402 = Self::wago402(&coupler);
+        for (port, state) in self.inputs.iter_mut().enumerate() {
+            state.set(wago402.get_input(port).unwrap_or(false));
+        }
+        Ok(())
+    }
+}
 
 impl Wago750_402Machine {
-    /// Safe to unwrap because we validated the type in build().
-    fn wago402(coupler: &mut Wago750_354) -> &mut Wago750_402 {
+    fn wago402(coupler: &Wago750_354) -> &Wago750_402 {
         coupler.slot_devices[0]
-            .as_mut()
+            .as_ref()
             .unwrap()
-            .as_any_mut()
-            .downcast_mut::<Wago750_402>()
+            .as_any()
+            .downcast_ref::<Wago750_402>()
             .unwrap()
-    }
-
-    fn update_led(&mut self, port: usize) -> ActResult {
-        let mut coupler = self.coupler.borrow_mut();
-        let value = Self::wago402(&mut coupler).get_input(port);
-        self.leds[port].set(value);
-        Ok(())
     }
 }
 
@@ -60,7 +61,6 @@ impl MachineBuild for Wago750_402Machine {
     fn build(ctx: &mut BuildContext) -> BuildResult<Self> {
         let (coupler, coupler_addr) = ctx.find_ethercat_device_and_addr::<Wago750_354>(0)?;
         let channel = ctx.get_ethercat_interface()?;
-
         {
             let mut c = coupler.borrow_mut();
             let modules = Wago750_354::initialize_modules(channel.clone(), coupler_addr)
@@ -69,37 +69,19 @@ impl MachineBuild for Wago750_402Machine {
                 c.set_module(module);
             }
             c.init_slot_modules(channel, coupler_addr);
-
-            // Validate type once at build time
-            let slot_dev = c.slot_devices[0]
-                .as_ref()
-                .expect("No device in slot 0: is the 750-402 registered in init_slot_modules?");
+            let slot_dev = c.slot_devices[0].as_ref().expect("No device in slot 0");
             assert!(
                 slot_dev.as_any().is::<Wago750_402>(),
                 "Slot 0 is not a Wago 750-402"
             );
         }
-
-        let led1 = ctx
-            .state::<bool>("led1_on")
-            .on_external_changed(|m: &mut Wago750_402Machine| m.update_led(0))
-            .build()?;
-        let led2 = ctx
-            .state::<bool>("led2_on")
-            .on_external_changed(|m: &mut Wago750_402Machine| m.update_led(1))
-            .build()?;
-        let led3 = ctx
-            .state::<bool>("led3_on")
-            .on_external_changed(|m: &mut Wago750_402Machine| m.update_led(2))
-            .build()?;
-        let led4 = ctx
-            .state::<bool>("led4_on")
-            .on_external_changed(|m: &mut Wago750_402Machine| m.update_led(3))
-            .build()?;
-
+        let in1 = ctx.state::<bool>("in1").build()?;
+        let in2 = ctx.state::<bool>("in2").build()?;
+        let in3 = ctx.state::<bool>("in3").build()?;
+        let in4 = ctx.state::<bool>("in4").build()?;
         Ok(Self {
             coupler,
-            leds: [led1, led2, led3, led4],
+            inputs: [in1, in2, in3, in4],
         })
     }
 }
